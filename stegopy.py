686e12b0ddd8d4870bdfea75e20bbd9759ee8bc7
Cyrill Leutwiler <bigcyrill@hotmail.com>
2016-07-18 13:45:14 +0200

    Create stegopy.py

---
+++ stegopy.py
@@ -0,0 +1,37 @@
+# Some python-fu I found useful for solving some stego ctf tasks
+
+# unsigned int to 8 digit binary string
+format(130, '08b')
+
+# signed 16bit int to binary
+format(130 & hex(2**16-1), 'b')
+
+# signed 32bit int to binary
+format(130 & hex(2**32-1), 'b')
+
+# hex touple to int
+struct.unpack("hh", str(frame))
+
+# lsb of a number
+lsb = number & 1
+
+# print binary string
+print ''.join(chr(int(data[i:i+8], 2)) for i in xrange(0, len(data), 8))
+
+# ternary operator in python
+a if c else b
+
+# loop through pixels of an image
+from PIL import Image, ImageFilter
+im = Image.open("img.png").convert("RGB")
+pixs = im.load()
+w, h = im.size
+for y in xrange(h):
+	for x in xrange(w):
+		pixel = pixs[x,y]
+
+# loop through frames of a (stereo) wave audio file
+import wave
+wave_file = wave.open('audio_file.wav', 'rb')
+for i in range(wave_file.getnframes()):
+  val = struct.unpack("hh", str(wave_file.readframes(1))) # convert frame to int values
